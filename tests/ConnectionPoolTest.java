07610bdcd1f84ae5bfb37edfd923c21a468b448b
Mordress <Mordress@gmail.com>
2015-11-29 19:32:21 +0300

    v. 0.2.1 tiny fixes

--- tests/ConnectionPoolTest.java
+++ tests/ConnectionPoolTest.java
@@ -1,17 +1,37 @@
 
 import com.gmail.mordress.lab4.dao.pool.ConnectionPool;
 import com.gmail.mordress.lab4.exceptions.PersistentException;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 
 public class ConnectionPoolTest {
 
     public static void main(String[] args) {
+        /*Logger logger = Logger.getLogger(ConnectionPoolTest.class);
+        logger.*/
+
         ConnectionPool pool = ConnectionPool.getInstance();
         try {
             pool.init();
             Connection connection = pool.getConnection();
-        } catch (PersistentException e) {
+            connection.close();
+            Connection con1 = pool.getConnection();
+            Connection con2 = pool.getConnection();
+            Connection con3 = pool.getConnection();
+            pool.destroy();
+            Connection con4 = pool.getConnection();
+            Connection con5 = pool.getConnection();
+            Connection con6 = pool.getConnection();
+            Connection con7 = pool.getConnection();
+            Connection con8 = pool.getConnection();
+            Connection con9 = pool.getConnection();
+            Connection con10 = pool.getConnection();
+            Connection con11 = pool.getConnection();
+            Connection con12 = pool.getConnection();
+            Connection con13 = pool.getConnection();
+            Connection con14 = pool.getConnection();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
