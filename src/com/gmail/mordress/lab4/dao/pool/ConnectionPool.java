89f1f3f220d49efb8da06ca11b7ab7715aec116a
Mordress <Mordress@gmail.com>
2015-11-29 18:43:57 +0300

    v. 0.1.3 connection pool

--- src/com/gmail/mordress/lab4/dao/pool/ConnectionPool.java
+++ src/com/gmail/mordress/lab4/dao/pool/ConnectionPool.java
@@ -2,7 +2,6 @@ package com.gmail.mordress.lab4.dao.pool;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,9 +14,7 @@ import com.gmail.mordress.lab4.exceptions.PersistentException;
 import org.apache.log4j.Logger;
 
 import java.util.Set;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public final class ConnectionPool {
 
@@ -35,7 +32,9 @@ public final class ConnectionPool {
 
     private int maxPoolSize;
 
-    private int checkConnectionTimeout;
+    private int minPoolSize;
+
+    private int timeout;
 
     private BlockingQueue<PooledConnection> freeConnections = new LinkedBlockingQueue<>();
 
@@ -53,28 +52,94 @@ public final class ConnectionPool {
             FileInputStream fileInputStream = new FileInputStream("resources" + File.separator + "db.properties");
             Properties properties = new Properties();
             properties.load(fileInputStream);
-
+            this.driverClass = properties.getProperty("DRIVER_CLASS");
+            this.dbURL = properties.getProperty("URL");
+            this.dbUser = properties.getProperty("USER");
+            this.dbPassword = properties.getProperty("PASSWORD");
+            this.maxPoolSize = Integer.parseInt(properties.getProperty("MAXPOOLSIZE"));
+            this.minPoolSize = Integer.parseInt(properties.getProperty("MINPOOLSIZE"));
+            this.timeout = Integer.parseInt(properties.getProperty("TIMEOUT"));
             Class.forName(driverClass);
-            this.url = url;
-            this.user = user;
-            this.password = password;
-            this.maxSize = maxSize;
-            this.checkConnectionTimeout = checkConnectionTimeout;
-            for(int counter = 0; counter < startSize; counter++) {
+            for(int counter = 0; counter < minPoolSize; counter++) {
                 freeConnections.put(createConnection());
             }
-        } catch (IOException e) {
-            logger.fatal("File db.properties not found.", e);
-            throw new PersistentException();
+            fileInputStream.close();
+        } catch (IOException | NumberFormatException e) {
+            logger.fatal("File db.properties not found or invalid.", e);
+            throw new PersistentException(e.getMessage(), e.getCause());
         } catch(ClassNotFoundException | SQLException | InterruptedException e) {
             logger.fatal("Can't initialize db connection pool.", e);
-            throw new PersistentException();
+            throw new PersistentException(e.getMessage(), e.getCause());
         }
     }
 
-    public synchronized Connection getConnection() throws ConnectionException {
+    public synchronized Connection getConnection() throws PersistentException {
+        PooledConnection connection = null;
+        while(connection == null) {
+            try {
+                if(!freeConnections.isEmpty()) {
+                    connection = freeConnections.take();
+                    if(!connection.isValid(timeout)) {
+                        try {
+                            connection.getConnection().close();
+                        } catch(SQLException e) {}
+                        connection = null;
+                    }
+                } else if(usedConnections.size() < maxPoolSize) {
+                    connection = createConnection();
+                } else {
+                    logger.error("The limit of number of database connections is exceeded");
+                    throw new PersistentException();
+                }
+            } catch(InterruptedException | SQLException e) {
+                logger.error("Can't connect to a database", e);
+                throw new PersistentException(e.getMessage(), e.getCause());
+            }
+        }
+        usedConnections.add(connection);
+        logger.debug(String.format("Connection was received from pool." +
+                " Current pool size: %d used connections; %d free connection",
+                usedConnections.size(), freeConnections.size()));
+        return connection;
 
     }
 
+    public synchronized void destroy() {
+        usedConnections.addAll(freeConnections);
+        freeConnections.clear();
+        for(PooledConnection connection : usedConnections) {
+            try {
+                connection.getConnection().close();
+            } catch(SQLException e) {}
+        }
+        usedConnections.clear();
+    }
 
+    @Override
+    protected void finalize() throws Throwable {
+        destroy();
+    }
+
+    private PooledConnection createConnection() throws SQLException {
+        return new PooledConnection(DriverManager.getConnection(dbURL, dbUser, dbPassword));
+    }
+
+    synchronized void freeConnection(PooledConnection connection) {
+        try {
+            if(connection.isValid(timeout)) {
+                connection.clearWarnings();
+                connection.setAutoCommit(true);
+                usedConnections.remove(connection);
+                freeConnections.put(connection);
+                logger.debug(String.format("Connection was returned into pool." +
+                        " Current pool size: %d used connections; %d free connection",
+                        usedConnections.size(), freeConnections.size()));
+            }
+        } catch(SQLException | InterruptedException e1) {
+            logger.warn("It is impossible to return database connection into pool", e1);
+            try {
+                connection.getConnection().close();
+            } catch(SQLException e2) {}
+        }
+    }
 }
