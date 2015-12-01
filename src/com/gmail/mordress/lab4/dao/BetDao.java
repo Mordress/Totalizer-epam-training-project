85feb6ac277af29d2fb99916a03ec65cdb309c4e
Mordress <Mordress@gmail.com>
2015-12-01 13:14:58 +0300

    v. 0.2.7 started impls

---
+++ src/com/gmail/mordress/lab4/dao/BetDao.java
@@ -0,0 +1,18 @@
+package com.gmail.mordress.lab4.dao;
+
+import com.gmail.mordress.lab4.domain.Bet;
+import com.gmail.mordress.lab4.domain.User;
+
+import java.util.List;
+
+public interface BetDao extends Dao<Bet> {
+
+    public List<Bet> findAllBetsByUser(User user) throws DaoException;
+
+    public List<Bet> findWinnedBets() throws DaoException;
+
+    public List<Bet> findWinnedBetsByUser(User user) throws DaoException;
+
+    public boolean isBetPassed(Bet bet);
+
+}
