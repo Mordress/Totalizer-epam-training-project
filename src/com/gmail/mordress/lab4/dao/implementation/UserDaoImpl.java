e5e7eaab58a83f47a77ae77bdca2931134918a99
Mordress <Mordress@gmail.com>
2015-12-06 13:56:21 +0300

    starting UserDaoImpl

--- src/com/gmail/mordress/lab4/dao/implementation/UserDaoImpl.java
+++ src/com/gmail/mordress/lab4/dao/implementation/UserDaoImpl.java
@@ -2,6 +2,7 @@ package com.gmail.mordress.lab4.dao.implementation;
 
 import com.gmail.mordress.lab4.dao.interfaces.UserDao;
 import com.gmail.mordress.lab4.domain.Bet;
+import com.gmail.mordress.lab4.domain.Role;
 import com.gmail.mordress.lab4.domain.User;
 import com.gmail.mordress.lab4.exceptions.DaoException;
 import org.apache.log4j.Logger;
@@ -19,12 +20,66 @@ public class UserDaoImpl extends BaseDaoImpl implements UserDao {
 
     @Override
     public User findUser(String login, String password) throws DaoException {
-        return null;
+        String sql = "SELECT * FROM `users` WHERE (`login` = ? AND `password` = ?)";
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, login);
+            statement.setString(2, password);
+            resultSet = statement.executeQuery();
+            User user = null;
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getInt("user_ID"));
+                user.setLogin(login);
+                user.setPassword(password);
+                user.setFirstName(resultSet.getString("first_name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setRole(Role.getByIdentity(resultSet.getInt("role")));
+                user.setEmail(resultSet.getString("email"));
+                user.setPhone(resultSet.getString("phone"));
+                user.setCashAmount(resultSet.getBigDecimal("cash_amount"));
+            }
+            logger.debug("User found.");
+            return user;
+        } catch (SQLException e) {
+            logger.error("Can not find user with login = " + login);
+            throw new DaoException(e.getMessage(), e.getCause());
+        } finally {
+            try {
+                resultSet.close();
+            } catch(SQLException | NullPointerException e) {}
+            try {
+                statement.close();
+            } catch(SQLException | NullPointerException e) {}
+        }
     }
 
     @Override
     public boolean checkUniqueLogin(String login) throws DaoException {
-        return false;
+        String sql = "SELECT `user_ID` FROM `users` WHERE `login` = ?";
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, login);
+            resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return false;
+            }
+            return true;
+        } catch (SQLException e) {
+            logger.debug("Can not check user-login for unique" + login);
+            throw new DaoException(e.getMessage(), e.getCause());
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException | NullPointerException e) {}
+            try {
+                statement.close();
+            } catch (SQLException | NullPointerException e) {}
+        }
     }
 
     @Override
