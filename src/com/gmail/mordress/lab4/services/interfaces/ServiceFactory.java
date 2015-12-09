3a56ed77012ce9834f9920e42b5c7d4e81176a02
Mordress <Mordress@gmail.com>
2015-12-09 23:14:25 +0300

    changed exceptions

--- src/com/gmail/mordress/lab4/services/interfaces/ServiceFactory.java
+++ src/com/gmail/mordress/lab4/services/interfaces/ServiceFactory.java
@@ -1,7 +1,6 @@
 package com.gmail.mordress.lab4.services.interfaces;
 
 import com.gmail.mordress.lab4.exceptions.PersistentException;
-import com.gmail.mordress.lab4.exceptions.ServiceException;
 
 public interface ServiceFactory {
     <Type extends Service> Type getService(Class<? extends Service> key) throws PersistentException;
