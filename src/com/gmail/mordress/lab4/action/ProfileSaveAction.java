b3892c7723e8e77a02cb8710c830e6aa092d7dec
Mordress <Mordress@gmail.com>
2015-12-19 19:25:10 +0300

    profile done!

--- src/com/gmail/mordress/lab4/action/ProfileSaveAction.java
+++ src/com/gmail/mordress/lab4/action/ProfileSaveAction.java
@@ -17,22 +17,29 @@ public class ProfileSaveAction extends AuthorizedUserAction {
         Action.Forward forward = new Action.Forward("/profile/edit.html");
         String oldPassword = request.getParameter("old-password");
         String newPassword = request.getParameter("new-password");
+        String confirmPassword = request.getParameter("confirm-password");
         User authorizedUser = getAuthorizedUser();
-        if(oldPassword != null && newPassword != null) {
-            UserService service = factory.getService(UserService.class);
-            User user = service.findUser(authorizedUser.getLogin(), oldPassword);
-            if(user != null) {
-                user.setPassword(newPassword);
-                service.save(user);
-                forward.getAttributes().put("message", "Пароль успешно изменён");
-                logger.info(String.format("User \"%s\" changed password", authorizedUser.getLogin()));
+        if(oldPassword != null && newPassword != null && confirmPassword != null) {
+            if (!newPassword.equals(confirmPassword)) {
+                forward.getAttributes().put("message", "Ошибка при наборе нового пароля");
+                logger.info(String.format("User \"%s\" tried to change password and specified the incorrect new password", authorizedUser.getLogin()));
             } else {
-                forward.getAttributes().put("message", "Старый пароль неопознан");
-                logger.info(String.format("User \"%s\" tried to change password and specified the incorrect previous password", authorizedUser.getLogin()));
+                UserService service = factory.getService(UserService.class);
+                User user = service.findUser(authorizedUser.getLogin(), oldPassword);
+                if (user != null) {
+                    user.setPassword(newPassword);
+                    service.save(user);
+                    forward.getAttributes().put("message", "Пароль успешно изменён");
+                    logger.info(String.format("User \"%s\" changed password", authorizedUser.getLogin()));
+                } else {
+                    forward.getAttributes().put("message", "Старый пароль неопознан");
+                    logger.info(String.format("User \"%s\" tried to change password and specified the incorrect previous password", authorizedUser.getLogin()));
+                }
             }
         } else {
             logger.warn(String.format("Incorrect data was found when user \"%s\" tried to change password", authorizedUser.getLogin()));
         }
+
         return forward;
     }
 }
